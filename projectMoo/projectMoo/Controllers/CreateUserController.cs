2037906788addf8adcbc86ffd744278f11d83911
Hjalti Steinar <hjalti15@ru.is>
2016-05-03 21:00:41 +0000

    .

---
+++ projectMoo/projectMoo/Controllers/CreateUserController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNet.Identity;
+using projectMoo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace projectMoo.Controllers
+{
+    public class CreateUserController : Controller
+    {
+        // GET: CreateUser
+        public ActionResult Index()
+        {
+            /*
+            var user = new ApplicationUser { UserName = "hjalti15@ru.is", Email = "hjalti15@ru.is" };
+            Microsoft.AspNet.Identity.UserManage;
+           */
+            return View();
+        }
+    }
+}
