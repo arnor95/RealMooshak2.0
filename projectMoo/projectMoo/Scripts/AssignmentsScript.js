327ee46a195f35c70e893e1ee980b240019533d9
Hjalti Steinar <hjalti15@ru.is>
2016-05-04 14:25:58 +0000

    javascript for assignment added to an external file

---
+++ projectMoo/projectMoo/Scripts/AssignmentsScript.js
@@ -0,0 +1,6 @@
+﻿$('.assignment-li-a').click(function (event) {
+    event.preventDefault();
+
+    var type = $(this).attr('data-type');
+    $("#" + type).removeClass("hidden").siblings("div").addClass("hidden");
+});
