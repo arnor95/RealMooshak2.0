7868006667b54d66a2a730282c0ed43b31cd52cc
Ármann Ari <armannari94@gmail.com>
2016-05-08 17:14:45 +0000

    validation error messages for createAssignmentView are in the house

--- projectMoo/projectMoo/Models/Entities/Assignment.cs
+++ projectMoo/projectMoo/Models/Entities/Assignment.cs
@@ -9,13 +9,18 @@ namespace projectMoo.Models.Entities
     public class Assignment
     {
         public int ID { get; set; }
+
         [Required(ErrorMessage = "Please select a course")]
         public int CourseID { get; set; }
+
         [Required(ErrorMessage = "Please enter a title for this assignment")]
         public string Title { get; set; }
+
         public decimal Grade { get; set; }
+
         [Required(ErrorMessage = "Please enter a descritption for this assignment")]
         public string Description { get; set; }
+
         [Required(ErrorMessage = "Please choose a due date for this assignment")]
         public DateTime DueDate { get; set; }
     }
