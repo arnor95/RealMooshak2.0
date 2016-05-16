f6b164a6aecd6d56ceb0842b0afa8b1c68e72505
arnor95 <arnorrg15@ru.is>
2016-05-16 17:38:39 +0000

    Fixed placeholders and changed update course user button

--- projectMoo/projectMoo/Models/ViewModels/UpdateCourseUsers.cs
+++ projectMoo/projectMoo/Models/ViewModels/UpdateCourseUsers.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,6 +8,7 @@ namespace projectMoo.Models.ViewModels
 {
     public class UpdateCourseUsers
     {
+        [Display(Name = "User Email")]
         public string UserID { get; set; }
         public string Course { get; set; }
     }
